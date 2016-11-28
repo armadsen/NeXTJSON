73ebc347128f4c71f1b77c3f15b828f1ddf4edee
Andrew Madsen <andrew@openreelsoftware.com>
2016-11-28 15:16:27 -0700

    Initial commit.

---
+++ Sources/NeXTJSON.h
@@ -0,0 +1,19 @@
+//
+//  NeXTJSON.h
+//  NeXTJSON
+//
+//  Created by Andrew Madsen on 11/28/16.
+//  Copyright © 2016 Open Reel Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NeXTJSON.
+FOUNDATION_EXPORT double NeXTJSONVersionNumber;
+
+//! Project version string for NeXTJSON.
+FOUNDATION_EXPORT const unsigned char NeXTJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeXTJSON/PublicHeader.h>
+
+
