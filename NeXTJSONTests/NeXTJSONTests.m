dbfec5c8d76f21191874b0c211a0aabfbdaf67de
Andrew Madsen <andrew@openreelsoftware.com>
2016-11-28 19:27:12 -0700

    Expand tests. Tests with all 6 test JSON files pass now.

--- NeXTJSONTests/NeXTJSONTests.m
+++ NeXTJSONTests/NeXTJSONTests.m
@@ -17,21 +17,27 @@
 
 - (void)test1
 {
-    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
-    NSURL *testFileURL = [bundle URLForResource:@"test1" withExtension:nil];
-    
-    NSData *testData = [NSData dataWithContentsOfURL:testFileURL];
-    XCTAssertNotNil(testData);
-    
-    id controlObject = [NSJSONSerialization JSONObjectWithData:testData options:0 error:NULL];
-    XCTAssertNotNil(controlObject);
-    
-    id testObject = [ORSJSONSerialization JSONObjectWithData:testData options:0];
-    XCTAssertNotNil(controlObject);
-    
-    XCTAssertEqualObjects(controlObject, testObject, @"Test object not equal to control object");
-    
-    NSLog(@"%@", testFileURL);
+	NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+	NSURL *testFilesFolderURL = [bundle URLForResource:@"tests" withExtension:nil];
+	NSArray *testFiles = [bundle URLsForResourcesWithExtension:@"json" subdirectory:[testFilesFolderURL lastPathComponent]];
+	for (NSURL *url in testFiles) {
+		[self _testParsingFileAtURL:url];
+	}
+}
+
+- (void)_testParsingFileAtURL:(NSURL *)url
+{
+	NSData *testData = [NSData dataWithContentsOfURL:url];
+	XCTAssertNotNil(testData, @"Unable to read data from %@", url);
+	
+	NSError *error = nil;
+	id controlObject = [NSJSONSerialization JSONObjectWithData:testData options:0 error:&error];
+	XCTAssertNotNil(controlObject, @"Unable to parse data from %@ with NSJSONSerialization: %@", url, error);
+	
+	id testObject = [ORSJSONSerialization JSONObjectWithData:testData options:0];
+	XCTAssertNotNil(controlObject);
+	
+	XCTAssertEqualObjects(controlObject, testObject, @"Test object not equal to control object for %@", url);
 }
 
 @end
